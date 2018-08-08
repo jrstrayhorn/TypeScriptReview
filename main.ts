e60cee9bc93a8b892f8bc06f7e18f699f8a1a31a
J.R. Strayhorn <jstrayhorn@chmhq.com>
2018-08-08 09:43:19 -0400

    done

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 // using IIFE to remove ts error about block scoping
 (function () {
 
+    // old school
+    let log = function(message) {
+        console.log(message);
+    }
 
+    // don't need curly branches / block if only one line
+    // always include parenthesis makes it more readable
+    let doLog = (message) => console.log(message);
     
 })();
