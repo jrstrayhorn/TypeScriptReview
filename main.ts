0c3bdb21e400d72e9e6dba372a52741a6ae46d17
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-08 15:55:23 -0400

    done

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
 // using IIFE to remove ts error about block scoping
 (function () {
 
+    // public, private, protected
+    // by default, everything is public
+    class Point {
+        private x: number;
+        private y: number;
 
+        constructor(x?: number, y?: number) {
+            this.x = x;
+            this.y = y;
+        }
+
+        draw() {
+            console.log(`X: ${this.x},Y: ${this.y}`);
+        }
+    }
+
+    let point = new Point(1, 2);
+    point.draw();
     
 })();
