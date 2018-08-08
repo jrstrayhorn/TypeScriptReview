a4ffb633570cf7c64a8e09865b94e6db78f1c375
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-08 15:47:47 -0400

    done

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 // using IIFE to remove ts error about block scoping
 (function () {
-
-    class Point {
-        x: number;
-        y: number;
-
+    var Point = /** @class */ (function () {
         // cant have multiple constructors in typescript.. only 1
         // once parameter is optional, all parameters afterwards must be optional
-        constructor(x?: number, y?: number) {
+        function Point(x, y) {
             this.x = x;
             this.y = y;
         }
-
-        draw() {
-            console.log(`X: ${this.x}, Y: ${this.y}`);
-        }
-    }
-
+        Point.prototype.draw = function () {
+            console.log("X: " + this.x + ", Y: " + this.y);
+        };
+        return Point;
+    }());
     // this code is verbose, we should use constructor instead
-    let point = new Point(1, 2);
+    var point = new Point(1, 2);
     point.draw();
-
-    let point2 = new Point();
+    var point2 = new Point();
     // don't need this verbose code
     //point.x = 1;
     //point.y = 2;
-    
-    
 })();
