83995c0dcfebf5c3297b6d0254c38cfbe23d6006
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-08 15:25:03 -0400

    done

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 // using IIFE to remove ts error about block scoping
 (function () {
 
+    // concept is Cohesion
+    // things that are related should go together
+    // we are using interface for data and method for drawing
+    // both the structure and method should go together
+    class Point {
+        // fields
+        x: number;
+        y: number;
 
+        // methods
+        draw() {
+            // ...
+        }
+
+        getDistance(another: Point) {
+            // ...
+        }
+    }
+
+    // class
+    // groups variables (properties) and functions that are highly related
+
+    // cant add function implementation to interfaces only contract of public methods or fields
     
 })();
