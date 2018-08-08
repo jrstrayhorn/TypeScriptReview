7966335b17ba088ab3057f1ea766889f058230de
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-08 16:00:02 -0400

    done

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
 // using IIFE to remove ts error about block scoping
 (function () {
 
+    class Point {
+        // now typescript compiler will automatically
+        // generate private fields for us
+        // will also initial fields with value of parameters
+        constructor(private x?: number, private y?: number) {
+        }
 
+        draw() {
+            console.log(`X: ${this.x}, Y: ${this.y}`);
+        }
+    }
+
+    let point = new Point(1, 2);
+    point.draw();
     
 })();
